c0fe92e9b325597577f6c164389b3e756cf19094
masoudmerzaye2020 <masoudmerzaye@gmail.com>
2023-12-23 15:25:12 -0800

    contribution error

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Resume from '../sections/resume';
 import Education from '@/sections/education';
 import Experience from '@/sections/experience';
 import Portfolio from '@/sections/portfolio';
-import Contribution from '@/sections/contribution';
+import Contribution from '../sections/contribution';
 
 const Home: NextPage = () => {
   return (
@@ -17,8 +17,6 @@ const Home: NextPage = () => {
       <Resume/>
       <Portfolio/>
       <Contribution/>
-
-      
     </div>
   )
 }

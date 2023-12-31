e91df7b627bc929418e9b88f220d8e8b31ebfbc4
masoudmerzaye2020 <masoudmerzaye@gmail.com>
2023-12-31 12:55:46 -0800

    update of contact

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@300&family=Orbitron:wght@400;700&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@300&family=Orbitron&display=swap" rel="stylesheet"></link>
